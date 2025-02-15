8cf61b2712fb269e7fb28f710ee196c5b39a8d33
as9305741 <as9305741@gmail.com>
2025-02-15 18:12:48 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
+  API_ID = int(os.environ.get("24436191", ""))
+  API_HASH = os.environ.get("959ad78ceb5d8446c65912be5117ac07", "")
+  BOT_TOKEN = os.environ.get("7677274761:AAHFYsvl3DXvLSnHG6Uaysgkt2XMh-sDcjg", "")
+  BOT_USERNAME = os.environ.get("Monster1838_bot", "")
+  DB_CHANNEL = int(os.environ.get("-1002432203859", ""))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  BOT_OWNER = int(os.environ.get("7511274110", ""))
+  DATABASE_URL = os.environ.get("mongodb+srv://as9305741:dlDXe8ix7LfZXHfh@cluster0.6iswx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "")
+  UPDATES_CHANNEL = os.environ.get("-1002432203859", "")
+  LOG_CHANNEL = int(os.environ.get("-1002432203859", ""))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -32,11 +32,11 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [monster](https://telegram.me/MONSTERGIVEAWAYSBOT)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](https://t.me/gojo_giveaways_bot)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
